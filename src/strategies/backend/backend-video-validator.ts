b361210cc4dff21c93bdd800ee390f00d13418d9
Pedro Tramontina <ptramontina@cloudgeometry.com>
2025-02-26 11:09:13 -0300

    Another test

--- src/strategies/backend/backend-video-validator.ts
+++ src/strategies/backend/backend-video-validator.ts
@@ -14,13 +14,11 @@ export class BackendVideoValidator {
     // const buffer = Buffer.from(newBuffer); // Can be used in  StreamBuffers.ReadableStreamBuffer readableStreamBuffer.put(buffer);
 
     if (typeof process !== "undefined") {
-      const backendVideoAnalyser = new BackendVideoAnalyser();
-      // backendVideoAnalyser.test();
-      const result = await backendVideoAnalyser.analyzeMediaBuffer(this.file);
-
-      console.log(result);
-
-      return true;
+      // const backendVideoAnalyser = new BackendVideoAnalyser();
+      // // backendVideoAnalyser.test();
+      // const result = await backendVideoAnalyser.analyzeMediaBuffer(this.file);
+      // console.log(result);
+      // return true;
     }
 
     return false;
