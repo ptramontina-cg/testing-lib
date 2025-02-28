fe0fdd1a16168e902e8c6e6ffa582a9c1dd94143
Pedro Tramontina <ptramontina@cloudgeometry.com>
2025-02-28 09:04:52 -0300

    Get video width and height

--- src/strategies/frontend/frontend-video-validator.ts
+++ src/strategies/frontend/frontend-video-validator.ts
@@ -1,6 +1,25 @@
 export class FrontendVideoValidator {
   async validate(file: File) {
     console.log("frontend validate video", file);
+
+    const video = await this.getImageFromFile(file);
+    console.log(video.videoWidth);
+    console.log(video.videoHeight);
+
     return true;
+  }
+
+  private async getImageFromFile(file: File): Promise<HTMLVideoElement> {
+    return new Promise((resolve, reject) => {
+      file.arrayBuffer().then((arrayB) => {
+        const blob = new Blob([arrayB], { type: file.type });
+        const url = URL.createObjectURL(blob);
+
+        let video = document.createElement("video");
+
+        video.onloadedmetadata = () => resolve(video);
+        video.src = url;
+      });
+    });
   }
 }
